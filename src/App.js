494076c6e80430bea1d2bcfc2f27771d76603d21
yaminhossen <streatdot616@gmail.com>
2022-05-04 23:18:03 +0600

    set a dynamic route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home/Home';
 import Header from './Pages/Shared/Header/Header';
 import Footer from './Pages/Shared/Footer/Footer';
+import Inventory from './Pages/Inventory/Inventory';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/inventory/:inventoryID' element={<Inventory></Inventory>}></Route>
       </Routes>
       <Footer> </Footer>
     </div>
--- src/Pages/Home/Item/Item.js
+++ src/Pages/Home/Item/Item.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Item.css'
 
 const Item = ({ item }) => {
     const { _id, name, price, img } = item;
+    const navigate = useNavigate();
+    const navigateToInventory = (_id) => {
+        navigate(`/inventory/${_id}`)
+    }
+
     return (
         <div className='item'>
             <h3>Name: {name}</h3>
             <p>Price:{price}</p>
             <img src={img} alt="" />
             <br />
-            <button>Details of: {name}</button>
+            <button onClick={() => navigateToInventory(_id)}>Details of: {name}</button>
         </div>
     );
 };
