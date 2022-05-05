4e95f5c84d562a8faa8ffe7ef660669e37bf0257
yaminhossen <streatdot616@gmail.com>
2022-05-05 13:02:28 +0600

    register form create and toggle

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './Register.css'
 
 const Register = () => {
+    const navigate = useNavigate();
+    const navigateLogin = () => {
+        navigate('/login')
+    }
     return (
-        <div>
-            <h2>please register</h2>
+        <div className='register-form'>
+            <h2 className='text-center'>please register</h2>
+            <form>
+                <input type="text" name="name" id="" placeholder='Your Name' />
+                <input type="email" name="email" id="" placeholder='Email Address' required />
+                <input type="password" name="password" placeholder='Password' id="" required />
+                <input type="submit" value="Register" />
+            </form>
+            <p>Already have an account? <Link to="/login" className='text-danger text-decoration-none' onClick={navigateLogin}>Please Login</Link></p>
         </div>
     );
 };
