0582be65b7a151523f59d0539e21d760ee3840e3
yaminhossen <streatdot616@gmail.com>
2022-05-08 01:15:10 +0600

    successfully item added server

--- src/Pages/AddItem/AddItem.js
+++ src/Pages/AddItem/AddItem.js
@@ -3,7 +3,22 @@ import { useForm } from "react-hook-form";
 
 const AddItem = () => {
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        console.log(data)
+        const url = `http://localhost:5000/item`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+            })
+    };
+
     return (
         <div className='w-50 mx-auto'>
             <h2>Add your items</h2>
@@ -12,9 +27,9 @@ const AddItem = () => {
                 <input className='mb-2' placeholder='Price' type="number" {...register("price")} />
                 <textarea className='mb-2' placeholder='Description'  {...register("description")} />
                 <input className='mb-2' placeholder='Quantity' type="number" {...register("quantity")} />
-                <input className='mb-2' placeholder='Suplier' type="number" {...register("suplier")} />
+                <input className='mb-2' placeholder='Suplier' type="text" {...register("suplier")} />
                 <input className='mb-2' placeholder='Photo URL' type="text" {...register("img")} />
-                <input type="submit" />
+                <input type="submit" value="Add Item" />
             </form>
         </div>
     );
