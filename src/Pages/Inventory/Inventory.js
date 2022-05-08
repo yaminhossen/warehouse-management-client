368f8e61b4853c40731c9401a94869c43708268a
yaminhossen <streatdot616@gmail.com>
2022-05-08 11:56:34 +0600

    item add api created

--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -1,3 +1,4 @@
+import { stringify } from '@firebase/util';
 import React, { useEffect, useState } from 'react';
 import { Nav } from 'react-bootstrap';
 import { Link, useParams } from 'react-router-dom';
@@ -12,21 +13,60 @@ const Inventory = () => {
             .then(res => res.json())
             .then(data => setItem(data));
     }, [])
+    console.log(item.quantity);
+    const handleIcrease = event => {
+        event.preventDefault();
+        const quantity = event.target.quantity.value;
+        // const iquantity = parseInt(item.quantity) + parseInt(quantity);
+        // const pquantity = JSON.stringify(iquantity);
+        // console.log(iquantity);
+        const updatedItem = { quantity };
+        console.log(updatedItem);
+
+        // send data to the server side
+        const url = `http://localhost:5000/item/${inventoryID}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updatedItem)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('success', data);
+                alert('quantity increase succesfully');
+                event.target.reset();
+            })
+    }
     return (
         <div>
             <div className='text-center mb-4'>
                 <h2>Wellcome to my inventoiry page:{item.name}</h2>
+                <div className=' w-50 mx-auto'>
+                    <div className='item'>
+                        <img src={item.img} alt="" />
+                        <br />
+                        <h3>Name: {item.name}</h3>
+                        <h5>Id: {item._id}</h5>
+                        <h4>Suplier: {item.suplier}</h4>
+                        <h6>Price:{item.price}</h6>
+                        <h6>Quantity: {item.quantity}</h6>
+                        <p><>Description:{item.description}</></p>
+                        <div className='text-center mb-4'>
+                            <button className=' bg-warning border border-redius-4'>Delivered</button>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className='text-center mb-4'>
-                <button className=' bg-warning border border-redius-4'>Delivered</button>
-            </div>
+
             <div className='text-center border w-50 mx-auto'>
                 <h3>Increase the quantity</h3>
-                <form className='text-center'>
-                    <input type="number" name="number" placeholder='Input the quantity' id="" />
+                <form onSubmit={handleIcrease} className='text-center'>
+                    <input type="number" name="quantity" placeholder='Input the quantity' id="" />
                     <br />
                     <br />
-                    <input className='bg-warning border-0' type="submit" value="submit" />
+                    <input className='bg-warning border-0' type="submit" value="Increase Quantity" />
                 </form>
             </div>
             <div>
--- src/Pages/MyItem/MyItem.js
+++ src/Pages/MyItem/MyItem.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyItem = () => {
+    const [user] = useAuthState(auth);
+    const [items, setItems] = useState([]);
+    useEffect(() => {
+        const getOrders = async () => {
+            const email = user.email;
+            const url = `http://localhost:5000/item?email=${email}`;
+            const { data } = await axios.get(url);
+            setItems(data);
+        }
+        getOrders();
+    }, [])
     return (
         <div>
-            <h3>this is my item</h3>
+            <h3>this is my item:{items.length}</h3>
+            <h2>{user.email}</h2>
         </div>
     );
 };
